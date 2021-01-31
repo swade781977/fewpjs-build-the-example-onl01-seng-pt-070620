89c528dde1f0a4e2f102a9660bf15f7fe1dc35c8
swade781977 <swade781977@yahoo.com>
2021-01-30 19:23:57 -0500

    commit tree 3af83cf0f7d6e20f61e644b2a824a7ca16b9fd7e

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.querySelector("#modal");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector("#modal");
+  let hearts = document.getElementsByClassName("like-glyph");
+  likePost(hearts)
+})
 
 // Your JavaScript code goes here!
+
+const likePost= function(hearts) {
+  for(const heart of hearts){
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+      .then(resp => {
+        if(heart.innerHTML == EMPTY_HEART){
+          heart.innerHTML = FULL_HEART
+          heart.className = "activated-heart"
+        }
+        else {
+          heart.innerHTML = EMPTY_HEART
+          heart.className = "like-glyph"
+        }
+      })
+      .catch(error => {
+        modal.hidden = false;
+        const modalMessage = document.querySelector("#modal-message")
+        modalMessage.innerText = error
+        setTimeout(() => {
+          modal.hidden = true;
+        },5000)
+      })
+    })
+  }
+}
+
+
 
 
 
